28bdd0f6c947d3fb794857f09013f9195525f92e
jovyn <jovyn4590@gmail.com>
2020-01-18 20:04:51 +0530

    Day 4 - guess game exercise

---
+++ guess_game.py
@@ -0,0 +1,20 @@
+'''
+Write a program to take 2 nos as input and the program will return a prompt to guess a value.
+if the no. matches the random generated no. we get a point.
+'''
+
+import random
+import sys
+
+val = random.randint(int(sys.argv[1]),int(sys.argv[2]))
+#print(val)  # Cheating
+guess = input("Guess a no: ")
+if val == int(guess):
+    print(f"Perfect! ")
+
+else:
+    print("Try again !!")
+
+
+
+
