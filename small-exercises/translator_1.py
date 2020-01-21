31158da956435a6091796514eba303fcdececec1
jovyn <jovyn4590@gmail.com>
2020-01-21 21:03:13 +0530

    re-arranges the code

--- small-exercises/translator_1.py
+++ small-exercises/translator_1.py
@@ -9,15 +9,15 @@ from googletrans import Translator
 translator = Translator()
 jokes = pyjokes.get_jokes()
 for joke in jokes:
-    with open('jokes/jokes_en.txt',mode='a') as out:
+    with open('jokes/jokes_en.txt', mode='a') as out:
         out.write(joke + "\n")
 
-read_jokes = open('jokes/jokes_en.txt',mode='r').readlines()
+read_jokes = open('jokes/jokes_en.txt', mode='r').readlines()
 
 for line in read_jokes :
     german_line = translator.translate(line, dest='de')
 
-    with open('jokes/jokes_de.txt',mode='a') as out:
+    with open('jokes/jokes_de.txt', mode='a') as out:
         out.write(german_line.text + "\n")
 
 
